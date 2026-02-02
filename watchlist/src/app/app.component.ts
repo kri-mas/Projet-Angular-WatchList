d6e271a094a2a4d1eec3651fbef902e8a9e3df8d
Souad Boussaghir <souadboussaghir@gmail.com>
2026-02-02 15:25:25 +0100

    Ajout de movie card et configuration de l'app

--- watchlist/src/app/app.component.ts
+++ watchlist/src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MovieListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'watchlist';
 }
+
+
+
+
+
+
+
+
+
