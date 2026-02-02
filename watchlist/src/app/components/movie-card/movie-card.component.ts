e37b65ac20f2076c569ee69325c65d7051c35923
Latifa Mahdad <latifamahdad293@gmail.com>
2026-02-02 21:31:01 +0100

    ajouter modif

--- watchlist/src/app/components/movie-card/movie-card.component.ts
+++ watchlist/src/app/components/movie-card/movie-card.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Movie } from '../../models/movie.model';
 
 @Component({
   selector: 'app-movie-card',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './movie-card.component.html',
-  styleUrl: './movie-card.component.css'
+  styleUrls: ['./movie-card.component.css']
 })
 export class MovieCardComponent {
   @Input({ required: true }) movie!: Movie;
@@ -13,4 +15,11 @@ export class MovieCardComponent {
   @Output() delete = new EventEmitter<number>();
   @Output() rate = new EventEmitter<number>();
   @Output() toggle = new EventEmitter<number>();
+
+  isWatched = false; // <-- état local pour la carte
+
+  onToggleWatched() {
+    this.isWatched = !this.isWatched;  // bascule le style et le badge
+    this.toggle.emit(this.movie.id);    // notifie le parent
+  }
 }
