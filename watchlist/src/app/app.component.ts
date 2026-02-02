775a3961fcb11717f793628d0723705426995f9a
Latifa Mahdad <latifamahdad293@gmail.com>
2026-02-02 15:44:56 +0100

    modif

--- watchlist/src/app/app.component.ts
+++ watchlist/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MovieListComponent } from '../app/components/movie-list/movie-list.component';  // <-- importer le composant
+import { HeaderComponent } from './components/header/header.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MovieListComponent],  // <-- ajouter ici
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'] // <-- attention, c'est styleSUrls et non styleUrl
+  imports: [HeaderComponent, MovieListComponent],
+  template: `
+    <app-header></app-header>
+    <app-movie-list></app-movie-list>
+  `
 })
-export class AppComponent {
-  }
-
+export class AppComponent {}
